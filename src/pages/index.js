994ff46c79076248e2df6898b38ee9e7dec73135
Arjun Sawhney <arjun7692@gmail.com>
2019-10-22 20:18:43 +0530

    chore(project): remove more unused boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello World</h1>
-  </Layout>
+    <Layout>
+        <section className="intro-title-wrapper">
+            <h1 className="heading">Arjun Sawhney</h1>
+            <h2 className="sub-heading">front-end engineer</h2>
+        </section>
+    </Layout>
 )
 
 export default IndexPage
