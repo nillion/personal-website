7fb4a9f224da8026c833f388ba693647171c7944
Arjun Sawhney <arjun7692@gmail.com>
2019-10-22 21:52:39 +0530

    feat(typewriter): use new typewriter UI component on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import Layout from '../components/layout';
+import Layout from '../components/app/layout';
+import TypeWriter from '../components/ui/TypeWriter';
 
 const IndexPage = () => (
     <Layout>
         <section className="intro-title-wrapper">
             <h1 className="heading">Arjun Sawhney</h1>
-            <h2 className="sub-heading">front-end engineer</h2>
+            <h2 className="sub-heading">
+                <TypeWriter text="front-end engineer" speed={100} />
+            </h2>
         </section>
     </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
